1e85528522e0cdcef0130e6c763c0686943d3ff9
Mohab Khaled <mohabk044@gmail.com>
2025-01-30 20:53:35 +0200

    Q2. Return the first product whose Price > 1000, unless there is no match, in which case null is returned.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -7,7 +7,11 @@ namespace Assignment
         {
             #region LINQ - Element Operators
             #region Q1- Get first Product out of Stock 
-            var Result = ProductList.First(P => P.UnitsInStock == 0);
+            //var Result = ProductList.First(P => P.UnitsInStock == 0);
+            //Console.WriteLine(Result);
+            #endregion
+            #region Q2. Return the first product whose Price > 1000, unless there is no match, in which case null is returned.
+            var Result = ProductList.FirstOrDefault(P => P.UnitPrice > 1000);
             Console.WriteLine(Result);
             #endregion
             #endregion
