c6f25a0f3fc36076bccded606ed2282aba5180b0
Mohab Khaled <mohabk044@gmail.com>
2025-01-30 20:52:16 +0200

    Q1- Get first Product out of Stock

---
+++ Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using static Assignment.ListGenerator;
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region LINQ - Element Operators
+            #region Q1- Get first Product out of Stock 
+            var Result = ProductList.First(P => P.UnitsInStock == 0);
+            Console.WriteLine(Result);
+            #endregion
+            #endregion
+        }
+    }
+}
